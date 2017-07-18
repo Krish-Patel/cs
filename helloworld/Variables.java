5cac205933c5885c360941618590f5fb0f3155a3
Krish-Patel <Krishmpatel04@gmail.com>
2017-07-18 18:29:39 -0400

    Learn about variabless

---
+++ helloworld/Variables.java
@@ -0,0 +1,18 @@
+public class Variables{
+	public static void main(String args[]) {
+		int a =7;
+		System.out.println(a);
+
+		char b='t';
+		System.out.println(b);
+
+		float c=9.4f;
+		System.out.println(c);
+
+		long d= 0123456L;
+		System.out.println(d) ;
+
+		String k= "Lightsabers";
+		System.out.println(k);
+	}
+}
